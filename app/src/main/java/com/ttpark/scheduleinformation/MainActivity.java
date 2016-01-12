7e40d640db02d800ca17d62ce2d1a4f2e0e2b2bf
Legen <644643473@qq.com>
2016-01-12 13:07:23 +0800

    20160112T13:06 addBasicWeeknum

--- app/src/main/java/com/ttpark/scheduleinformation/MainActivity.java
+++ app/src/main/java/com/ttpark/scheduleinformation/MainActivity.java
@@ -1,10 +1,46 @@
 package com.ttpark.scheduleinformation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends Activity {
+
+    private int year_c = 0;
+    private int month_c = 0;
+    private int day_c = 0;
+    private int week_c = 0;
+    private int week_num = 0;
+    private String currentDate = "";
+    private int currentYear;
+    private int currentMonth;
+    private int currentDay;
+    private int currentWeek;
+    private int currentNum;
+
+
+
+/*****
+ *
+ * 日期计算主程序
+ *
+ *  **/
+    public MainActivity(){
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-M-d");
+        currentDate = sdf.format(date);
+        year_c = Integer.parseInt(currentDate.split("-")[0]);
+        month_c = Integer.parseInt(currentDate.split("-")[1]);
+        day_c = Integer.parseInt(currentDate.split("-")[2]);
+        currentYear = year_c;
+        currentMonth = month_c;
+        currentDay = day_c;
+        
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
